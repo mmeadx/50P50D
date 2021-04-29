e568f3cbbfee6b447cf37785c9d8312a45767002
mmead <mmeadx@gmail.com>
2021-04-29 15:52:55 -0500

    hidden search widget done

---
+++ hiddenSeachWidget/script.js
@@ -0,0 +1,10 @@
+console.log("script.js running");
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
